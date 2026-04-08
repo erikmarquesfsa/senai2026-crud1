3d5620d1eaf3400c4d761732c2a2be0b38798d85
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2026-04-08 20:03:38 -0300

    d

--- listar_produtos.php
+++ listar_produtos.php
@@ -18,32 +18,10 @@ include "scripts/functions.php";
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                </ul>
-                <b class="ms-auto">Seja bem vindo <?php echo dadosCadastro($_SESSION['usuario']); ?></b>
-                &nbsp;<a href="index.php" class="btn btn-danger">Sair</a>
-            </div>
-        </div>
-    </nav>
+    <?php include "menu.php"; ?>
     <div class="container">
         <h1>Nossos Produtos</h1>
-        <table class="table">
+        <table class="table table-striped text-center align-middle">
             <thead>
                 <tr>
                     <th scope="col">#</th>
---
+++ menu.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="dashboard.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.php">Cadastrar Produtos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="listar_produtos.php">Listar Produtos</a>
+                    </li>
+                </ul>
+                <b class="ms-auto">Seja bem vindo <?php echo dadosCadastro($_SESSION['usuario']); ?></b>
+                &nbsp;<a href="index.php" class="btn btn-danger">Sair</a>
+            </div>
+        </div>
+    </nav>
