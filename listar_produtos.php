2bb8a8b8b276685d403b6bc3b5ac3cc3df8c8458
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2026-04-08 19:20:51 -0300

    d

--- listar_produtos.php
+++ listar_produtos.php
@@ -54,13 +54,19 @@ include "scripts/functions.php";
                 </tr>
             </thead>
             <tbody>
+                <?php
+                include("config/conexao.php");
+                $result = $conn->query("SELECT * FROM produtos");
+                while($row = $result->fetch_assoc()){
+                ?>
                 <tr>
                     <td>1</td>
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
                     <td>@mdo</td>
-                </tr>               
+                </tr>
+                <?php } ?>               
             </tbody>
         </table>
     </div>
